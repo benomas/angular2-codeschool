ec856c031be5774880748e821123a11fd45ce3af
Benomas <benomas@gmail.com>
2016-07-01 23:21:39 -0500

    video 1

--- app/main.ts
+++ app/main.ts
@@ -1,2 +1,23 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+
+
+@Component(
+    {
+        selector:'my-app',
+        template:'<h1>{{title}}</h1>'
+    });
+class AppComponent
+{
+    title = 'Utra Raicing';
+
+    carPart =
+    {
+        "id":1,
+        "name":"Super Tires",
+        "description":"These tires are the very best",
+
+    }
+}
+
+bootstrap(AppComponent);
